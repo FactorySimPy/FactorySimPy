7cbb8ae7bac0709e3ba8471466bd25a26cc766a1
lekshmi <lekshminair0503@gmail.com>
2025-05-04 15:04:51 +0530

    changes in readme.md and getting_started.md

--- examples/quick_start.py
+++ examples/quick_start.py
@@ -6,18 +6,18 @@ import factorysimpy
 env = simpy.Environment()
 
 #Initializing nodes
-m1 = Processor(env, name="Drill-1", proc_time=3)   # 3 time‑unit cycle
-m2 = Processor(env, name="Grind-1", proc_time=lambda: random.gauss(4, 0.5))
+m1 = Processor(env, name="Drill-1", delay=3)   # 3 time‑unit cycle
+m2 = Processor(env, name="Grind-1", delay=0.3)
 
 #Initializing edges
-belt = Conveyor(env, capacity=10, speed=1.2)            # units per time‑step
-buffer1 = Buffer(env, name="buffer-1", capacity=5, delay=0.5)
-buffer2 = Buffer(env, name="buffer-2", capacity=5, delay=0.5)
+belt = Conveyor(env, belt_capacity=10, delay_per_slot=1.2)            # units per time‑step
+buffer1 = Buffer(env, name="buffer-1", store_capacity=5, delay=0.5)
+buffer2 = Buffer(env, name="buffer-2", store_capacity=5, delay=0.5)
 
 
 
 # initializing source and sink
-src  = Source(env, interarrival= 2)
+src  = Source(env, delay= 2)
 sink = Sink(env)
 
 
