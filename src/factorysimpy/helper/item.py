6d6060bc21ea33ed75dd223195a300fbc3501ba4
lekshmi <lekshminair0503@gmail.com>
2025-06-07 00:52:46 +0530

    documentation update

--- src/factorysimpy/helper/item.py
+++ src/factorysimpy/helper/item.py
@@ -2,6 +2,11 @@ class Item:
     """A class representing an item with a 'ready' flag."""
     def __init__(self, id):
         self.id = id
+        self.timestamp_creatiom_ = None
+        self.timestamp_destruction = None
+        self.timestamp_node_entry = None
+        self.timestamp_node_exit = None
+        self.payload = None
         
 
     def __repr__(self):
